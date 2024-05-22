536b7c773252b151d13613272179e8b3caea57f2
GianBaeza <gianb04@gmail.com>
2024-05-22 18:57:53 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const pantalla = document.getElementById("pantalla");
+const botones = document.querySelectorAll("button");
+
+botones.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    const botonApretado = boton.textContent;
+    if (boton.id === "c") {
+      pantalla.textContent = "0";
+      return;
+    }
+    if (boton.id === "igual") {
+      try {
+        pantalla.textContent = eval(pantalla.textContent);
+        pantalla.textContent.length > 10 ? Math.floor(pantalla.textContent) : pantalla.textContent
+      } catch {
+        pantalla.textContent = "Erro!";
+      }
+    }
+
+    pantalla.textContent === "0"
+      ? (pantalla.textContent = botonApretado)
+      : (pantalla.textContent += botonApretado);
+  });
+});
